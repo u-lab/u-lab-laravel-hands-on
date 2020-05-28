9da78bb4c57bbe5facbafa4698c5028637a7624b
H37kouya <h37kouya@gmail.com>
2020-05-28 21:04:30 +0900

    feat HelloControllerの作成

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/v1/hello', 'HelloController@index');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
